5e0c4f186893db526f1875fb1a9163fbfcf9ddba
conti.davide <conti.davide@HP-005>
2025-11-06 10:48:18 +0100

    Aggiungere i file di progetto.

---
+++ es_rosso_array/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace es_rosso_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] v1 = { 2, 5, 7, 8, 3 };
+            int [] v2 = { 2, 6, 1, 8, 5 };
+
+            int contV3 = 0;
+            
+            for(int i=0; i<v1.Length; i++)
+            {
+                for(int j=0; j<v2.Length; j++)
+                {
+                    if (v1[i] == v2[j])
+                    {
+                        contV3 = contV3 + 1;
+                    }
+                }
+                
+            }
+
+            int[] v3 = new int[contV3];
+
+            int indV3 = 0;
+
+            for(int i=0; i<v3.Length; i++)
+            {
+                for (int j = 0; j < v1.Length; j++)
+                {
+                    for(int k=0; k<v2.Length; k++)
+                    {
+                        if (v1[j] == v2[k])
+                        {
+                            v3[indV3] = v1[j];
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < v3.Length; i++) {
+
+                Console.Write($"[{v3[i]}]");
+
+            }
+        }
+    }
+}
